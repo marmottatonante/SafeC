0f6e1033045b8bc33ef2c995e4032f2f1ce18edd
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2025-10-22 23:50:59 +0200

    Good version of safeptr.

--- safeptr.c
+++ safeptr.c
@@ -4,40 +4,31 @@
 
 static const safeptr DEFAULT_SAFEPTR =
 {
+    .state = SAFEPTR_STATE_UNALLOCATED,
     .data = NULL,
-    .size = 0
+    .size = 0,
 };
 
-safeptr_result safeptr_alloc(safeptr* p, void* data, size_t size)
+safeptr safeptr_create()
 {
-    if(p == NULL || data == NULL || size == 0)
-        return SAFEPTR_ERROR_INVALID_ARGUMENT;
-    
-    *p = DEFAULT_SAFEPTR;
-
-    void* temp_data = malloc(size);
-    if(temp_data == NULL)
-        return SAFEPTR_ERROR_ALLOC_FAIL;
-
-    p->data = temp_data;
-    p->size = size;
-
-    memcpy(p->data, data, p->size);
-
-    return SAFEPTR_SUCCESS;
+    return DEFAULT_SAFEPTR;
 }
 
-safeptr_result safeptr_realloc(safeptr *p, size_t new_size)
+safeptr_result safeptr_alloc(safeptr* p, size_t size)
 {
-    if(p == NULL || new_size == 0)
+    if(p == NULL || size == 0)
         return SAFEPTR_ERROR_INVALID_ARGUMENT;
     
-    void* new_data = realloc(p->data, new_size);
-    if(new_data == NULL)
-        return SAFEPTR_ERROR_SIZE_UNCHANGED;
+    if(p->state != SAFEPTR_STATE_UNALLOCATED)
+        return SAFEPTR_ERROR_INVALID_STATE;
     
-    p->data = new_data;
-    p->size = new_size;
+    void* data = malloc(size);
+    if(data == NULL)
+        return SAFEPTR_ERROR_ALLOCATION_FAILED;
+
+    p->state = SAFEPTR_STATE_UNINITIALIZED;
+    p->data = data;
+    p->size = size;
 
     return SAFEPTR_SUCCESS;
 }
@@ -46,8 +37,55 @@ safeptr_result safeptr_free(safeptr* p)
 {
     if(p == NULL)
         return SAFEPTR_ERROR_INVALID_ARGUMENT;
+    
+    if(p->state == SAFEPTR_STATE_UNALLOCATED)
+        return SAFEPTR_ERROR_INVALID_STATE;
 
     free(p->data);
     *p = DEFAULT_SAFEPTR;
+
     return SAFEPTR_SUCCESS;
+}
+
+safeptr_result safeptr_realloc(safeptr *p, size_t new_size)
+{
+    if(p == NULL)
+        return SAFEPTR_ERROR_INVALID_ARGUMENT;
+        
+    if(p->state == SAFEPTR_STATE_UNALLOCATED)
+        return SAFEPTR_ERROR_INVALID_STATE;
+    
+    if(new_size == 0)
+        return safeptr_free(p);
+
+    void* new_data = realloc(p->data, new_size);
+    if(new_data == NULL)
+        return SAFEPTR_ERROR_SIZE_UNCHANGED;
+        
+    size_t old_size = p->size;
+    if(new_size > old_size)
+        p->state = SAFEPTR_STATE_UNINITIALIZED;
+    p->data = new_data;
+    p->size = new_size;
+
+    return SAFEPTR_SUCCESS;
+}
+
+safeptr_result safeptr_copy(safeptr* p, void* data)
+{
+    if(p == NULL || data == NULL)
+        return SAFEPTR_ERROR_INVALID_ARGUMENT;
+    
+    if(p->state == SAFEPTR_STATE_UNALLOCATED)
+        return SAFEPTR_ERROR_INVALID_STATE;
+    
+    memcpy(p->data, data, p->size);
+    p->state = SAFEPTR_STATE_INITIALIZED;
+    
+    return SAFEPTR_SUCCESS;
+}
+
+bool safeptr_is_initialized(safeptr* p)
+{
+    return p != NULL && p->state == SAFEPTR_STATE_INITIALIZED;
 }
--- safeptr.h
+++ safeptr.h
@@ -2,9 +2,20 @@
 #define SAFEPTR_H
 
 #include <stddef.h>
+#include <stdbool.h>
+
+enum safeptr_state_e
+{
+    SAFEPTR_STATE_UNALLOCATED = 0, // Memory has not been allocated. Invalid pointer.
+    SAFEPTR_STATE_UNINITIALIZED, // Memory has not been initialized and may contain garbage.
+    SAFEPTR_STATE_INITIALIZED, // Memory has been initialized and pointer is readable.
+};
+
+typedef enum safeptr_state_e safeptr_state;
 
 struct safeptr_s
 {
+    safeptr_state state;
     void* data;
     size_t size;
 };
@@ -13,16 +24,20 @@ typedef struct safeptr_s safeptr;
 
 enum safeptr_result_e
 {
-    SAFEPTR_SUCCESS = 0, // Success, pointer is valid
-    SAFEPTR_ERROR_INVALID_ARGUMENT, // Invalid argument, pointer is invalid
-    SAFEPTR_ERROR_ALLOC_FAIL, // Alloc failed, pointer is invalid
-    SAFEPTR_ERROR_SIZE_UNCHANGED // Realloc failed, but pointer is still valid
+    SAFEPTR_SUCCESS = 0, // Operation succeded
+    SAFEPTR_ERROR_INVALID_ARGUMENT, // An invalid argument was passed to the function
+    SAFEPTR_ERROR_INVALID_STATE, // Pointer is in an invalid state for the operation
+    SAFEPTR_ERROR_ALLOCATION_FAILED, // Memory allocation failed 
+    SAFEPTR_ERROR_SIZE_UNCHANGED, // Memory reallocation failed, but pointer is still valid
 };
 
 typedef enum safeptr_result_e safeptr_result;
 
-safeptr_result safeptr_alloc(safeptr* p, void* data, size_t size);
-safeptr_result safeptr_realloc(safeptr *p);
+safeptr safeptr_create();
+safeptr_result safeptr_alloc(safeptr* p, size_t size);
 safeptr_result safeptr_free(safeptr* p);
+safeptr_result safeptr_realloc(safeptr *p, size_t new_size);
+safeptr_result safeptr_copy(safeptr* p, void* data);
+bool safeptr_is_initialized(safeptr* p);
 
 #endif
