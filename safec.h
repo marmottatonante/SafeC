bc0e9868b66b57837bc12c6bc8e81e9c57aac657
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2025-10-26 17:18:03 +0100

    Moved everything into a safec header and source file.

--- safec.h
+++ safec.h
@@ -1,17 +1,70 @@
 #ifndef SAFEC_H
 #define SAFEC_H
 
-enum safec_result_e
+#include <stddef.h>
+#include <stdbool.h>
+#include <threads.h>
+
+/*
+    safeerr (Error Management)
+*/
+
+enum safeerr_e
 {
-    SAFEC_SUCCESS = 0, // Operation succeded
+    SAFEERR_SUCCESS = 0, // Operation succeded
 
-    SAFEC_ERROR_INVALID_ARGUMENT, // An invalid argument was passed to the function
-    SAFEC_ERROR_INVALID_STATE, // Pointer is in an invalid state for the operation
+    SAFEERR_INVALID_ARGUMENT,
 
-    SAFEC_ERROR_CONCURRENCY_FAIL, // Initializing or locking/unlocking the mutex failed
-    SAFEC_ERROR_ALLOCATION_FAILED, // Memory allocation failed, malloc returned NULL
+    SAFEERR_MUTEX_INIT_FAILED,
+    SAFEERR_MUTEX_LOCK_FAILED,
+    SAFEERR_MUTEX_UNLOCK_FAILED,
+    SAFEERR_MUTEX_IS_INVALID,
+
+    SAFEERR_MALLOC_FAILED,
+    SAFEERR_REALLOC_FAILED,
 };
 
-typedef enum safec_result_e safec_result;
+typedef enum safeerr_e safeerr;
+
+safeerr safeerr_read(void);
+
+/*
+    safesyn (Thread Safety)
+*/
+
+struct safesyn_s
+{
+    mtx_t lock;
+    bool is_valid;
+    bool is_enabled;
+};
+
+typedef struct safesyn_s safesyn;
+
+bool safesyn_create(safesyn* sync, bool is_enabled);
+bool safesyn_lock(safesyn* sync);
+bool safesyn_unlock(safesyn* sync);
+bool safesyn_destroy(safesyn* sync);
+
+/*
+    safemem (Safe Memory Block)
+*/
+
+struct safemem_s
+{
+    safesyn sync;
+
+    void* data;
+    size_t size;
+    bool is_managed;
+};
+
+typedef struct safemem_s safemem;
+
+bool safemem_create(safemem* new_safemem, const size_t size, const bool is_managed, const bool is_concurrent);
+bool safemem_destroy(safemem* ptr);
+bool safemem_resize(safemem *ptr, const size_t new_size);
+bool safemem_read(safemem* ptr, void* out_data);
+bool safemem_write(safemem* ptr, const void* new_data, const size_t new_size);
 
 #endif
