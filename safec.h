7e475bbfaf7cdeaaff45172204da2372368f845d
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2025-10-27 11:35:26 +0100

    Removed useless safres struct. Managing errors with safcod enum now.

--- safec.h
+++ safec.h
@@ -6,65 +6,71 @@
 #include <threads.h>
 
 /*
-    safeerr (Error Management)
+    safcod (Error Management)
 */
 
-enum safeerr_e
+enum safcod_e
 {
-    SAFEERR_SUCCESS = 0, // Operation succeded
+    SAFCOD_SUCCESS = 0,
 
-    SAFEERR_INVALID_ARGUMENT,
-
-    SAFEERR_MUTEX_INIT_FAILED,
-    SAFEERR_MUTEX_LOCK_FAILED,
-    SAFEERR_MUTEX_UNLOCK_FAILED,
-    SAFEERR_MUTEX_IS_INVALID,
-
-    SAFEERR_MALLOC_FAILED,
-    SAFEERR_REALLOC_FAILED,
+    _SAFCOD_WARNING_START, // internal
+    SAFCOD_WARNING_MUTEX_UNLOCK_FAILED,
+    SAFCOD_WARNING_REALLOC_FAILED,
+    _SAFCOD_WARNING_END, // internal
+    
+    _SAFCOD_ERROR_START, // internal
+    SAFCOD_ERROR_INVALID_ARGUMENT,
+    SAFCOD_ERROR_MUTEX_INIT_FAILED,
+    SAFCOD_ERROR_MUTEX_LOCK_FAILED,
+    SAFCOD_ERROR_MUTEX_IS_INVALID,
+    SAFCOD_ERROR_MALLOC_FAILED,
+    _SAFCOD_ERROR_END, // internal
 };
 
-typedef enum safeerr_e safeerr;
+typedef enum safcod_e safcod;
 
-safeerr safeerr_read(void);
+inline bool safcod_is_success(safcod code);
+inline bool safcod_is_warning(safcod code);
+inline bool safcod_is_error(safcod code);
+inline bool safcod_is_ok(safcod code);
 
 /*
-    safesyn (Thread Safety)
+    safsyn (Thread safty)
 */
 
-struct safesyn_s
+struct safsyn_s
 {
     mtx_t lock;
     bool is_valid;
     bool is_enabled;
 };
 
-typedef struct safesyn_s safesyn;
+typedef struct safsyn_s safsyn;
 
-bool safesyn_create(safesyn* sync, bool is_enabled);
-bool safesyn_lock(safesyn* sync);
-bool safesyn_unlock(safesyn* sync);
-bool safesyn_destroy(safesyn* sync);
+safcod safsyn_create(safsyn* sync, bool is_enabled);
+safcod safsyn_lock(safsyn* sync);
+safcod safsyn_unlock(safsyn* sync);
+safcod safsyn_destroy(safsyn* sync);
 
 /*
-    safemem (Safe Memory Block)
+    safmem (Memory Block)
 */
 
-struct safemem_s
+struct safmem_s
 {
-    safesyn sync;
+    safsyn sync;
 
     void* data;
     size_t size;
     bool is_managed;
 };
 
-typedef struct safemem_s safemem;
+typedef struct safmem_s safmem;
 
-bool safemem_create(safemem* new_safemem, const size_t size, const bool is_managed, const bool is_concurrent);
-bool safemem_destroy(safemem* ptr);
-bool safemem_resize(safemem *ptr, const size_t new_size);
-bool safemem_read(safemem* ptr, void* out_data);
-bool safemem_write(safemem* ptr, const void* new_data, const size_t new_size);
+safcod safmem_create(safmem* new_safmem, const size_t size, const bool is_managed, const bool is_concurrent);
+safcod safmem_destroy(safmem* ptr);
+safcod safmem_resize(safmem *ptr, const size_t new_size);
+safcod safmem_read(safmem* ptr, void* out_data);
+safcod safmem_write(safmem* ptr, const void* new_data, const size_t new_size);
 
 #endif
